55a5b3f1d2c431ee7b69c4d6f533e2ff1a7a8f27
algotrading123456789 <aimdse3@gmail.com>
2024-05-12 19:46:13 +0530

    Update Bhaisaab.py

--- Bhaisaab.py
+++ Bhaisaab.py
@@ -59,15 +59,19 @@ def job():
 
     # Verification
     if __name__ == "__main__":
-        vix_india_ohcs = vix_india(starting_date="25-04-2024")
-        data_vix = vix_india_ohcs.fetch_vix_data()
+    vix_india_ohcs = vix_india(starting_date="25-04-2024")
+    data_vix = vix_india_ohcs.fetch_vix_data()
 
-    dataframe_vix = pd.DataFrame(data_vix)
-    Nifty_Vix = dataframe_vix.iloc[-1, 7]
-    Nifty_Vix_Y = dataframe_vix.iloc[-2, 7]
-    D_Vix = Nifty_Vix - Nifty_Vix_Y
-    Diff_Vix = "{:.2f}%".format(D_Vix)
-
+    if not data_vix.empty:
+        dataframe_vix = pd.DataFrame(data_vix)
+        Nifty_Vix = dataframe_vix.iloc[-1, 7]
+        Nifty_Vix_Y = dataframe_vix.iloc[-2, 7]
+        D_Vix = Nifty_Vix - Nifty_Vix_Y
+        Diff_Vix = "{:.2f}%".format(D_Vix)
+        return dataframe_vix
+    else:
+        print("Dataframe is empty.")
+        return pd.DataFrame()
 
     class SpotPrice:
         def __init__(self, identifier="NIFTY 50", name="NIFTY 50", timeout=15):
